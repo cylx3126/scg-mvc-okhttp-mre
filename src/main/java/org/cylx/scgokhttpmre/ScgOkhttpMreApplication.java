2082f430adf448bb4f423f08c581daa41c69a4b1
cylx <49175642+cylx3126@users.noreply.github.com>
2024-03-13 10:00:54 +0800

    init

---
+++ src/main/java/org/cylx/scgokhttpmre/ScgOkhttpMreApplication.java
@@ -0,0 +1,13 @@
+package org.cylx.scgokhttpmre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgOkhttpMreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgOkhttpMreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cylx/scgokhttpmre/ScgOkhttpMreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cylx.scgokhttpmre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgOkhttpMreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
